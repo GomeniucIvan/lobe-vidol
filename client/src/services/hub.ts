07433e4f6b2edb6a7b0bf71aeaf063751138b5a7
GomeniucIvan <gomeniucivan@gmail.com>
2024-05-22 23:24:27 +0300

    fix header

--- client/src/services/hub.ts
+++ client/src/services/hub.ts
@@ -1,9 +1,13 @@
 import {HubModelIndex} from "@/types/hub";
 import {ServerConfig} from "@/types/config";
+import {createHeader} from "@/store/config/selectors/config";
 
 class HubService {
   getModelList = async (config: ServerConfig): Promise<HubModelIndex> => {
-    const res = await fetch(`${config?.endpoint}download/list`);
+    const res = await fetch(`${config?.endpoint}download/list`, {
+      headers: createHeader(),
+      method: 'GET',
+    });
 
     return res.json();
   };
